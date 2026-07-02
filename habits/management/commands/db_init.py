cae07c97e0989dde91a3481e3628f228d894b4dd
Rychard <rychard.famichou@gmail.com>
2026-07-02 15:30:26 +0200

    First check

---
+++ habits/management/commands/db_init.py
@@ -0,0 +1,44 @@
+import psycopg
+from psycopg import sql
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Создает базу данных PostgreSQL, если она еще не существует'
+
+    def handle(self, *args, **options):
+        db_settings = settings.DATABASES.get('default', {})
+
+        target_db = db_settings.get('NAME')
+        db_user = db_settings.get('USER')
+        db_password = db_settings.get('PASSWORD')
+        db_host = db_settings.get('HOST')
+        db_port = db_settings.get('PORT')
+
+        self.stdout.write(f"Подключение к PostgreSQL для создания базы '{target_db}'...")
+
+        try:
+            with psycopg.connect(
+                    dbname='postgres',
+                    user=db_user,
+                    password=db_password,
+                    host=db_host,
+                    port=db_port,
+                    autocommit=True
+            ) as conn:
+
+                with conn.cursor() as cur:
+                    cur.execute("SELECT 1 FROM pg_database WHERE datname = %s;", (target_db,))
+                    exists = cur.fetchone()
+
+                    if not exists:
+                        cur.execute(
+                            sql.SQL("CREATE DATABASE {};").format(sql.Identifier(target_db))
+                        )
+                        self.stdout.write(self.style.SUCCESS(f"База данных '{target_db}' успешно создана!"))
+                    else:
+                        self.stdout.write(f"База данных '{target_db}' уже существует.")
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Не удалось создать базу данных. Ошибка: {e}"))
---
+++ habits/tasks.py
@@ -0,0 +1,19 @@
+from celery import shared_task
+from celery.utils.log import get_task_logger
+from django.utils import timezone
+
+from habits.services import send_telegram_notification
+
+logger = get_task_logger(__name__)
+
+@shared_task
+def remind_habit():
+    chat_id = "@RychardFamichou"
+    text = "Пить воду"
+    now = timezone.localtime(timezone.now())
+
+    if now.hour in (6, 12, 18):
+        send_telegram_notification(chat_id, text)
+
+    logger.info("!!! СРАБОТАЛА ЗАДАЧА REMIND_HABIT !!!")
+    return "Успешно выполнено!"
